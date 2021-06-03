4edae72bdf030e7c832d07008b0ef3bcc6aa4fdf
Stephen Crane <scrane88@gmail.com>
2021-06-03 16:39:21 -0400

    added like capabilities with error handling

--- main.js
+++ main.js
@@ -3,7 +3,51 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.querySelector('#modal')
+  const modalMessage = document.querySelector('#modal-message')
+  const likes = [...document.getElementsByClassName('like-glyph')]
 
+
+  likes.forEach( like => {
+    like.addEventListener('click', () => {
+      mimicServerCall()
+      .then( () => {
+        if (like.innerText === EMPTY_HEART){
+          like.innerText = FULL_HEART
+          like.classList.add('activated-heart')
+        } else {
+          like.innerText = EMPTY_HEART
+          like.classList.remove('activated-heart')
+        }
+      })
+      .catch(error => {
+        errorModal.classList.remove('hidden')
+        modalMessage.innerText = error
+        setTimeout(() => {
+          errorModal.classList.add('hidden')
+          modalMessage.innerText = ''
+        }, 3000)
+      })
+    })
+  })
+  /*
+    grab the error modal and modal message
+    get array of all likes
+    iterate through and create event listeners for each like
+      mimicServerCall acts as a fetch
+        .catch failure status
+          remove hidden from error modal
+          display message in the modal
+          setTimeout to hide the modal after 3 seconds
+        if success and heart is empty
+          change heart glyph to a full heart
+          add .activated-heart class
+        else
+          change heart glyph to empty
+          remove .activated-heart class
+  */
+})
 
 
 
